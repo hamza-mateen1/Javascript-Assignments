b1d2244ed4084025c2f654d3a4c0fe47e61fa648
Muhammad Hamza Mateen <50620040+hamza-mateen1@users.noreply.github.com>
2023-05-30 22:36:04 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,133 @@
-// 1. Write a script to greet your website visitor using JS alert box.
+// 1. Declare and initialize an empty multidimensional array. (Array of arrays)
 
-alert("Assalam o Alikum Every One");
+var arr = [[],[]];
 
-// 2. Write a script to display following message on your web page:
+// 2. Declare and initialize a multidimensional array representing the following matrix:
 
-alert("Error! Please Enter a valid password.");
+arr[0]=[0,1,2,3];
+arr[1]=[1,0,1,2];
+arr[2]=[2,1,0,1];
+document.write(arr[0]+"<br>"+arr[1]+"<br>"+arr[2]);
 
-// 3. Write a script to display following message on your web page: (Hint : Use line break)
+// 3 Write a program to print numeric counting from 1 to 10.
 
-alert("Welcome to JS Land...\n Happy Coding!");
+for (var index = 1; index <= 10; index++) {
+     console.log(index);
 
-// 4. Write a script to display following messages in sequence:
+}
 
-alert("Welcome to JS Land...");
+// 4. Write a program to print multiplication table of any number using for loop. Table number & length should be taken as an input from user
 
-alert(" Happy Coding!");
+var userNum = +prompt("Enter a number to show its multiplications table");
+ var userleng = +prompt("Enter length multiplications table");
+document.write("Multiplication table of "+ userNum+ " Length of " +userleng +"<br>");
 
-5. Generate the following message through browser’s developer console:
+ for (var i = 1; i<= userleng; i++ ){
+document.write(userNum + " * " + i + " = " +  userNum*i +"<br>" )
 
-alert("Hello.. I can run JS through my web Browser's console");
+ }
+
+// 5. Write a program to print items of the following array using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”, “strawberry”]
+
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+ for (var i = 0; i < fruits.length; i++) {
+    document.write(fruits[i]+"<br>");   
+    }
+    for (var i = 0; i < fruits.length; i++) {   
+        console.log("Element at index "+ i + " is "+ fruits[i]);
+       }
+
+// 6. Generate the following series in your browser. See example output.
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
 
 
-console.log("Hello.. I can run JS through my web Browser's console");
+document.write("<h3>Counting:</h3>")
+for (let index = 1; index <= 15; index++) {
+    document.write([index] + ",");
+
+}
 
 
-// 6. Make use of alerts in your new /existing HTML & CSS project.
+// // b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 
 
-// 7. Practice placement of < script ></script > element in
-//     following sections of your project in exercise 6:
-a.Head
-b.Body(before your page’s HTML)
-c. Body (inside your page’s HTML)
-d. Body (after your page’s HTML)
+document.write("<h3>Reverse Counting:</h3>")
+for (let index = 10; index >= 1; index--) {
+    document.write([index] + ",");
+
+}
+
+
+// // c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+
+
+document.write("<h3>EVEN:</h3>")
+for (let index = 1; index <= 15; index++){
+    if (index % 2 == 0) {
+        document.write(index + ",");
+        }
+}
+// d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+document.write("<h3>ODD:</h3>")
+for (let index = 1; index <= 15; index++){
+    if (index % 2 == 1) {
+        document.write(index + ",");
+    }}
+
+// e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+document.write("<h3>Series: </h3>")
+for (let index = 1; index <= 15; index++){
+    if (index % 2 == 0) {
+        document.write(index + "k,");
+    }}
+
+// 7. You have an array
+// A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”]
+//Write a program to enable “search by user input” in an array.
+
+var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+var userInput = prompt("welcome to ABC Bakery. What do you want to order sir/ma'am");
+var matchfound = false;
+for (var index = 0; index < A.length; index++) {
+    if (A[index] == userInput) {
+        matchfound = true
+document.write(userInput+" is <b>available</b> at index "+index+" in our bakery");
+    }
+}
+if (matchfound === false) {
+         document.write("We are Sorry. "+userInput+ " is <b>not available</b> in our bakery"); }
+
+// 8. Write a program to identify the largest number in the given array.
+
+
+A = [24, 53, 78, 91, 12]
+var largest = A[0];
+for (var index = 0; index < A.length; index++) {
+    if (A[index] > largest) {
+        largest = A[index];
+    }
+}
+document.write("Array items: " + A);
+document.write("<br>The largest number is " + largest);
+
+// 9. Write a program to identify the smallest number in the given array.
+
+A = [24, 53, 78, 91, 12]
+var smallest = A[0];
+for (var index = 0; index < A.length; index++) {
+    if (A[index] < smallest) {
+        smallest = A[index];
+        }
+        }
+        document.write("Array items: " + A);
+        document.write("<br>The smallest number is " + smallest);
+
+
+
+// 10. Write a program to print multiples of 5 ranging 1 to 100.
+
+ for (var index = 1; index <= 100; index++) {
+    if (index % 5 == 0) {
+   document.write(index + ", ");}
+ }
